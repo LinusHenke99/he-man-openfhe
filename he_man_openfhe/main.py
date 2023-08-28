d78a599b85c897fb08632054c32f45cc328a761e
Linus Henke <linus.henke@mci.edu>
2023-08-28 16:32:55 +0200

    Implemented Batch-Normalization

---
+++ he_man_openfhe/main.py
@@ -0,0 +1,144 @@
+import sys
+from typing import Any, Callable
+
+import click
+import numpy as np
+from loguru import logger
+
+import neuralpy
+
+from he_man_openfhe import config, crypto
+from he_man_openfhe.inference import ONNXModel
+from pydantic_settings import BaseSettings
+
+
+def config_args(cfg_class: BaseSettings) -> Callable:
+    def annotator(function: Callable) -> Callable:
+        for key in cfg_class.__fields__:
+            if hasattr(cfg_class, "_%s" % key):
+                function = getattr(cfg_class, "_%s" % key)(function)
+        return function
+
+    return annotator
+
+
+@click.group()
+@click.version_option()
+@click.option("-v", "--verbose", count=True, help="set the verbosity, can be repeated")
+@logger.catch
+def command_line(verbose: int) -> None:
+    """A CLI tool for model inference with TenSEAL"""
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        level={0: "ERROR", 1: "WARNING", 2: "INFO", 3: "DEBUG", 4: "TRACE"}[verbose],
+    )
+
+
+@command_line.command()
+@config_args(config.KeyParamsConfig)
+@logger.catch
+def keyparams(**kwargs: Any) -> None:
+    """Generates HE keys"""
+    try:
+        cfg = config.KeyParamsConfig(**kwargs)
+        run_keyparams(cfg)
+    except Exception as e:
+        raise click.ClickException(str(e))
+
+
+@command_line.command()
+@config_args(config.KeyGenConfig)
+@logger.catch
+def keygen(**kwargs: Any) -> None:
+    """Generates HE keys"""
+    try:
+        cfg = config.KeyGenConfig(**kwargs)
+        run_keygen(cfg)
+    except Exception as e:
+        raise click.ClickException(str(e))
+
+
+@command_line.command()
+@config_args(config.EncryptConfig)
+@logger.catch
+def encrypt(**kwargs: Any) -> None:
+    """Encrypts model input"""
+    try:
+        cfg = config.EncryptConfig(**kwargs)
+        run_encrypt(cfg)
+    except Exception as e:
+        raise click.ClickException(str(e))
+
+
+@command_line.command()
+@config_args(config.InferenceConfig)
+@logger.catch
+def inference(**kwargs: Any) -> None:
+    """Applies a model to an encrypted input"""
+    try:
+        cfg = config.InferenceConfig(**kwargs)
+        run_inference(cfg)
+    except Exception as e:
+        raise click.ClickException(str(e))
+
+
+@command_line.command()
+@config_args(config.DecryptConfig)
+@logger.catch
+def decrypt(**kwargs: Any) -> None:
+    """Decrypts model output"""
+    try:
+        cfg = config.DecryptConfig(**kwargs)
+        run_decrypt(cfg)
+    except Exception as e:
+        raise click.ClickException(str(e))
+
+
+#   Does probably not require changes
+def run_keyparams(cfg: config.KeyParamsConfig) -> None:
+    model = ONNXModel(cfg.model_path, cfg)
+    key_params = crypto.find_optimal_parameters(cfg, model)
+    key_params.save(cfg.key_params_path)
+    model.save_calibrated_model()
+
+
+#   Does probably not require changes
+def run_keygen(cfg: config.KeyGenConfig) -> None:
+    key_params = crypto.KeyParams.load(cfg.key_params_path)
+    context = crypto.create_context(key_params)
+    crypto.save_context(context, cfg.secret_key_path)
+
+
+def run_encrypt(cfg: config.EncryptConfig) -> None:
+    context = crypto.load_context(cfg.key_path, operation=False)
+    plaintext = np.load(cfg.plaintext_input_path)
+    plaintext = list(plaintext.flat)
+    input_size = len(plaintext)
+    pl = context.context.PackPlaintext(plaintext)
+    ciphertext = context.context.Encrypt(pl, context.public_key)
+    ciphertext.setSlots(input_size)
+    crypto.save_vector(ciphertext, cfg.ciphertext_output_path)
+
+
+#   TODO:   Context deserialization needs to be handled here
+def run_inference(cfg: config.InferenceConfig) -> None:
+    model = ONNXModel(cfg.model_path)
+    context = crypto.load_context(cfg.key_path)
+    neuralpy.SetContext(context.context)
+    input = crypto.load_vector(cfg.ciphertext_input_path)
+    output = model(input)[0]
+    crypto.save_vector(output, cfg.ciphertext_output_path)
+
+
+def run_decrypt(cfg: config.DecryptConfig) -> None:
+    context = crypto.load_context(cfg.key_path, operation=False)
+    if not context.private_key:
+        raise ValueError("No private key available in {}".format(str(cfg.key_path)))
+
+    ciphertext = crypto.load_vector(cfg.ciphertext_input_path)
+    output_size = ciphertext.getSlots()
+    plaintext = context.context.Decrypt(ciphertext, context.private_key)
+    plaintext.SetLength(output_size)
+    plaintext = np.array(plaintext.GetPackedValue())
+    np.save(cfg.plaintext_output_path, plaintext)
